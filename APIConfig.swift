d8321edc82ce269274a6b487fb72ad247364393f
Basheer Abdulmalik <basheer0abdulmalik@gmail.com>
2023-07-01 10:55:24 +0300

    start

---
+++ APIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  APIConfig.swift
+//  MarvelVerse
+//
+//  Created by Basheer Abdulmalik on 01/07/2023.
+//
+
+import Foundation
+
+let url = URL(string: "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=96cfa48ca9c0a2e2273c897356ba5f37&hash=18ee522a7cc80757a01ca3bb79608f05")
+let publicKey = "96cfa48ca9c0a2e2273c897356ba5f37"
+let privateKey = "77d97c0de2fdea9d41f80aba6e86069f7d5bb7ab"
+let timestamp = "1"
+let hash = "177d97c0de2fdea9d41f80aba6e86069f7d5bb7ab96cfa48ca9c0a2e2273c897356ba5f37"
---
+++ MarvelVerse/Main/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  MarvelVerse
+//
+//  Created by Basheer Abdulmalik on 30/06/2023.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+//        let url = URL(string: "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=96cfa48ca9c0a2e2273c897356ba5f37&hash=18ee522a7cc80757a01ca3bb79608f05")
+//        let publicKey = "96cfa48ca9c0a2e2273c897356ba5f37"
+//        let privateKey = "77d97c0de2fdea9d41f80aba6e86069f7d5bb7ab"
+//        let timestamp = "1"
+//        let hash = "177d97c0de2fdea9d41f80aba6e86069f7d5bb7ab96cfa48ca9c0a2e2273c897356ba5f37"
+    }
+}
